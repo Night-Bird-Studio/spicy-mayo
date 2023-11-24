df61b1d7af3be48cbb5c6794ba38c5162a4e702e
Sachi <30622947+Salayna@users.noreply.github.com>
2023-11-24 19:40:16 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/fr/niji/socle/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.night.bird.studio.spicymayo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/route_list.dart
@@ -0,0 +1,21 @@
+class RouteList {
+  RouteList._();
+
+  static const String splash = '/splash';
+  static const String home = '/movies';
+  static const String movie = '/movie';
+  static const String movieDetail = ':id';
+  static const String favorites = '/favorites';
+  static const String about = '/about';
+}
+
+class RouteNames {
+  RouteNames._();
+
+  static const String splash = 'Splash';
+  static const String home = 'Home';
+  static const String movie = 'Movie';
+  static const String about = 'About';
+  static const String movieDetail = 'MovieDetail';
+  static const String favorites = 'Favorites';
+}
